d4051acd7194f1f665ddd295e22d322b0ccaaabb
Winglite <wiglite@gmai.com>
2022-09-27 06:32:06 -0300

    aprendendo comando DML, view e Order by

--- Exercicios/Script Exercicio 3.sql
+++ Exercicios/Script Exercicio 3.sql
@@ -280,3 +280,386 @@ INNER JOIN TELEFONE T
 ON C.IDCLIENTE = T.ID_CLIENTE
 WHERE SEXO = 'F'
 AND ESTADO = 'SP'; 
+
+
+
+/* nome, email, numero, estado */
+
+SELECT C.NOME, C.EMAIL, E.ESTADO, T.NUMERO
+FROM CLIENTE C
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE;
+
++---------+-------------------+--------+----------+
+| NOME    | EMAIL             | ESTADO | NUMERO   |
++---------+-------------------+--------+----------+
+| JORGE   | JORGE@IG.COM      | ES     | 78458743 |
+| JOAO    | JOAOA@IG.COM      | RJ     | 87866896 |
+| CARLOS  | CARLOSA@IG.COM    | RJ     | 54768899 |
+| JOAO    | JOAOA@IG.COM      | RJ     | 99667587 |
+| ANA     | ANA@IG.COM        | SP     | 78989765 |
+| ANA     | ANA@IG.COM        | SP     | 99766676 |
+| JOAO    | JOAOA@IG.COM      | RJ     | 66687899 |
+| JORGE   | JORGE@IG.COM      | ES     | 89986668 |
+| CARLOS  | CARLOSA@IG.COM    | RJ     | 88687909 |
+| JORGE   | JORGE@IG.COM      | ES     | 56576876 |
+| FLAVIO  | FLAVIO@IG.COM     | MG     | 68976565 |
+| FLAVIO  | FLAVIO@IG.COM     | MG     | 99656675 |
+| GIOVANA | NULL              | RJ     | 33567765 |
+| GIOVANA | NULL              | RJ     | 88668786 |
+| GIOVANA | NULL              | RJ     | 55689654 |
+| KARLA   | KARLA@GMAIL.COM   | RJ     | 88687979 |
+| DANIELE | DANIELE@GMAIL.COM | ES     | 88965676 |
+| EDUARDO | NULL              | PR     | 89966809 |
+| ANTONIO | ANTONIO@IG.COM    | SP     | 88679978 |
+| ANTONIO | ANTONIO@UOL.COM   | PR     | 99655768 |
+| ELAINE  | ELAINE@GLOBO.COM  | SP     | 89955665 |
+| CARMEM  | CARMEM@IG.COM     | RJ     | 77455786 |
+| CARMEM  | CARMEM@IG.COM     | RJ     | 89766554 |
+| ADRIANA | ADRIANA@GMAIL.COM | RJ     | 77755785 |
+| ADRIANA | ADRIANA@GMAIL.COM | RJ     | 44522578 |
++---------+-------------------+--------+----------+
+
+
+SELECT C.NOME, 
+IFNULL(C.EMAIL,'NAO TEM EMAIL'), 
+E.ESTADO, 
+T.NUMERO
+FROM CLIENTE C
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE;
+
++---------+---------------------------------+--------+----------+
+| NOME    | IFNULL(C.EMAIL,'NAO TEM EMAIL') | ESTADO | NUMERO   |
++---------+---------------------------------+--------+----------+
+| JORGE   | JORGE@IG.COM                    | ES     | 78458743 |
+| JOAO    | JOAOA@IG.COM                    | RJ     | 87866896 |
+| CARLOS  | CARLOSA@IG.COM                  | RJ     | 54768899 |
+| JOAO    | JOAOA@IG.COM                    | RJ     | 99667587 |
+| ANA     | ANA@IG.COM                      | SP     | 78989765 |
+| ANA     | ANA@IG.COM                      | SP     | 99766676 |
+| JOAO    | JOAOA@IG.COM                    | RJ     | 66687899 |
+| JORGE   | JORGE@IG.COM                    | ES     | 89986668 |
+| CARLOS  | CARLOSA@IG.COM                  | RJ     | 88687909 |
+| JORGE   | JORGE@IG.COM                    | ES     | 56576876 |
+| FLAVIO  | FLAVIO@IG.COM                   | MG     | 68976565 |
+| FLAVIO  | FLAVIO@IG.COM                   | MG     | 99656675 |
+| GIOVANA | NAO TEM EMAIL                   | RJ     | 33567765 |
+| GIOVANA | NAO TEM EMAIL                   | RJ     | 88668786 |
+| GIOVANA | NAO TEM EMAIL                   | RJ     | 55689654 |
+| KARLA   | KARLA@GMAIL.COM                 | RJ     | 88687979 |
+| DANIELE | DANIELE@GMAIL.COM               | ES     | 88965676 |
+| EDUARDO | NAO TEM EMAIL                   | PR     | 89966809 |
+| ANTONIO | ANTONIO@IG.COM                  | SP     | 88679978 |
+| ANTONIO | ANTONIO@UOL.COM                 | PR     | 99655768 |
+| ELAINE  | ELAINE@GLOBO.COM                | SP     | 89955665 |
+| CARMEM  | CARMEM@IG.COM                   | RJ     | 77455786 |
+| CARMEM  | CARMEM@IG.COM                   | RJ     | 89766554 |
+| ADRIANA | ADRIANA@GMAIL.COM               | RJ     | 77755785 |
+| ADRIANA | ADRIANA@GMAIL.COM               | RJ     | 44522578 |
++---------+---------------------------------+--------+----------+
+
+SELECT C.NOME, 
+IFNULL(C.EMAIL,'**********') AS "E-MAIL", 
+E.ESTADO, 
+T.NUMERO
+FROM CLIENTE C
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE;
+
++---------+-------------------+--------+----------+
+| NOME    | E-MAIL            | ESTADO | NUMERO   |
++---------+-------------------+--------+----------+
+| JORGE   | JORGE@IG.COM      | ES     | 78458743 |
+| JOAO    | JOAOA@IG.COM      | RJ     | 87866896 |
+| CARLOS  | CARLOSA@IG.COM    | RJ     | 54768899 |
+| JOAO    | JOAOA@IG.COM      | RJ     | 99667587 |
+| ANA     | ANA@IG.COM        | SP     | 78989765 |
+| ANA     | ANA@IG.COM        | SP     | 99766676 |
+| JOAO    | JOAOA@IG.COM      | RJ     | 66687899 |
+| JORGE   | JORGE@IG.COM      | ES     | 89986668 |
+| CARLOS  | CARLOSA@IG.COM    | RJ     | 88687909 |
+| JORGE   | JORGE@IG.COM      | ES     | 56576876 |
+| FLAVIO  | FLAVIO@IG.COM     | MG     | 68976565 |
+| FLAVIO  | FLAVIO@IG.COM     | MG     | 99656675 |
+| GIOVANA | **********        | RJ     | 33567765 |
+| GIOVANA | **********        | RJ     | 88668786 |
+| GIOVANA | **********        | RJ     | 55689654 |
+| KARLA   | KARLA@GMAIL.COM   | RJ     | 88687979 |
+| DANIELE | DANIELE@GMAIL.COM | ES     | 88965676 |
+| EDUARDO | **********        | PR     | 89966809 |
+| ANTONIO | ANTONIO@IG.COM    | SP     | 88679978 |
+| ANTONIO | ANTONIO@UOL.COM   | PR     | 99655768 |
+| ELAINE  | ELAINE@GLOBO.COM  | SP     | 89955665 |
+| CARMEM  | CARMEM@IG.COM     | RJ     | 77455786 |
+| CARMEM  | CARMEM@IG.COM     | RJ     | 89766554 |
+| ADRIANA | ADRIANA@GMAIL.COM | RJ     | 77755785 |
+| ADRIANA | ADRIANA@GMAIL.COM | RJ     | 44522578 |
++---------+-------------------+--------+----------+
+
+
+/* VIEW */
+
+SELECT C.NOME, 
+	   C.SEXO, 
+	   C.EMAIL, 
+	   T.TIPO, 
+	   T.NUMERO, 
+	   E.BAIRRO, 
+	   E.CIDADE, 
+	   E.ESTADO
+FROM CLIENTE C
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE;
+
+CREATE VIEW V_RELATORIO AS
+SELECT C.NOME, 
+	   C.SEXO, 
+	   IFNULL(C.EMAIL,'CLIENTE SEM EMAIL') AS "E-MAIL", 
+	   T.TIPO, 
+	   T.NUMERO, 
+	   E.BAIRRO, 
+	   E.CIDADE, 
+	   E.ESTADO
+FROM CLIENTE C
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE;
+
+SHOW TABLES;
+
++--------------------+
+| Tables_in_comercio |
++--------------------+
+| cliente            |
+| endereco           |
+| telefone           |
+| v_relatorio        |
++--------------------+
+
+SELECT * FROM V_RELATORIO;
+
++---------+------+-------------------+------+----------+------------+----------------+--------+
+| NOME    | SEXO | E-MAIL            | TIPO | NUMERO   | BAIRRO     | CIDADE         | ESTADO |
++---------+------+-------------------+------+----------+------------+----------------+--------+
+| JORGE   | M    | JORGE@IG.COM      | CEL  | 78458743 | CENTRO     | VITORIA        | ES     |
+| JOAO    | M    | JOAOA@IG.COM      | CEL  | 87866896 | CENTRO     | RIO DE JANEIRO | RJ     |
+| CARLOS  | M    | CARLOSA@IG.COM    | COM  | 54768899 | ESTACIO    | RIO DE JANEIRO | RJ     |
+| JOAO    | M    | JOAOA@IG.COM      | RES  | 99667587 | CENTRO     | RIO DE JANEIRO | RJ     |
+| ANA     | F    | ANA@IG.COM        | CEL  | 78989765 | JARDINS    | SAO PAULO      | SP     |
+| ANA     | F    | ANA@IG.COM        | CEL  | 99766676 | JARDINS    | SAO PAULO      | SP     |
+| JOAO    | M    | JOAOA@IG.COM      | COM  | 66687899 | CENTRO     | RIO DE JANEIRO | RJ     |
+| JORGE   | M    | JORGE@IG.COM      | RES  | 89986668 | CENTRO     | VITORIA        | ES     |
+| CARLOS  | M    | CARLOSA@IG.COM    | CEL  | 88687909 | ESTACIO    | RIO DE JANEIRO | RJ     |
+| JORGE   | M    | JORGE@IG.COM      | RES  | 56576876 | CENTRO     | VITORIA        | ES     |
+| FLAVIO  | M    | FLAVIO@IG.COM     | RES  | 68976565 | CASCADURA  | B. HORIZONTE   | MG     |
+| FLAVIO  | M    | FLAVIO@IG.COM     | CEL  | 99656675 | CASCADURA  | B. HORIZONTE   | MG     |
+| GIOVANA | F    | CLIENTE SEM EMAIL | CEL  | 33567765 | CENTRO     | RIO DE JANEIRO | RJ     |
+| GIOVANA | F    | CLIENTE SEM EMAIL | CEL  | 88668786 | CENTRO     | RIO DE JANEIRO | RJ     |
+| GIOVANA | F    | CLIENTE SEM EMAIL | COM  | 55689654 | CENTRO     | RIO DE JANEIRO | RJ     |
+| KARLA   | F    | KARLA@GMAIL.COM   | COM  | 88687979 | COPACABANA | RIO DE JANEIRO | RJ     |
+| DANIELE | F    | DANIELE@GMAIL.COM | COM  | 88965676 | CENTRO     | VITORIA        | ES     |
+| EDUARDO | M    | CLIENTE SEM EMAIL | CEL  | 89966809 | CENTRO     | CURITIBA       | PR     |
+| ANTONIO | M    | ANTONIO@IG.COM    | COM  | 88679978 | JARDINS    | SAO PAULO      | SP     |
+| ANTONIO | M    | ANTONIO@UOL.COM   | CEL  | 99655768 | BOM RETIRO | CURITIBA       | PR     |
+| ELAINE  | F    | ELAINE@GLOBO.COM  | RES  | 89955665 | LAPA       | SAO PAULO      | SP     |
+| CARMEM  | F    | CARMEM@IG.COM     | RES  | 77455786 | CENTRO     | RIO DE JANEIRO | RJ     |
+| CARMEM  | F    | CARMEM@IG.COM     | RES  | 89766554 | CENTRO     | RIO DE JANEIRO | RJ     |
+| ADRIANA | F    | ADRIANA@GMAIL.COM | RES  | 77755785 | CENTRO     | RIO DE JANEIRO | RJ     |
+| ADRIANA | F    | ADRIANA@GMAIL.COM | COM  | 44522578 | CENTRO     | RIO DE JANEIRO | RJ     |
++---------+------+-------------------+------+----------+------------+----------------+--------+
+
+
+-- DROP VIEW V_RELATORIO;
+
+SELECT NOME, NUMERO, ESTADO
+FROM V_RELATORIO;
+
++---------+----------+--------+
+| NOME    | NUMERO   | ESTADO |
++---------+----------+--------+
+| JORGE   | 78458743 | ES     |
+| JOAO    | 87866896 | RJ     |
+| CARLOS  | 54768899 | RJ     |
+| JOAO    | 99667587 | RJ     |
+| ANA     | 78989765 | SP     |
+| ANA     | 99766676 | SP     |
+| JOAO    | 66687899 | RJ     |
+| JORGE   | 89986668 | ES     |
+| CARLOS  | 88687909 | RJ     |
+| JORGE   | 56576876 | ES     |
+| FLAVIO  | 68976565 | MG     |
+| FLAVIO  | 99656675 | MG     |
+| GIOVANA | 33567765 | RJ     |
+| GIOVANA | 88668786 | RJ     |
+| GIOVANA | 55689654 | RJ     |
+| KARLA   | 88687979 | RJ     |
+| DANIELE | 88965676 | ES     |
+| EDUARDO | 89966809 | PR     |
+| ANTONIO | 88679978 | SP     |
+| ANTONIO | 99655768 | PR     |
+| ELAINE  | 89955665 | SP     |
+| CARMEM  | 77455786 | RJ     |
+| CARMEM  | 89766554 | RJ     |
+| ADRIANA | 77755785 | RJ     |
+| ADRIANA | 44522578 | RJ     |
++---------+----------+--------+
+
+
+/* UPDATE, INSERT E DELETE - DML */
+
+INSERT INTO V_RELATORIO VALUES(
+'ANDREIA','F','ANDREIA@UOL.COM.BR','CEL','873547864','CENTRO','VITORIA','ES'
+);
+
+ERROR 1394 (HY000): Can not insert into join view 'comercio.v_relatorio' without fields list
+ERROR 1395 (HY000): Can not delete from join view 'comercio.v_relatorio'
+
+DELETE FROM V_RELATORIO WHERE NOME = 'JORGE';
+
+/* É PERMITIDO FAZER UPDATES EM VIEWS COM JOIN */
+
+UPDATE V_RELATORIO SET NOME = 'JOSE' WHERE NOME = 'JORGE';
+
+CREATE TABLE JOGADORES(
+	IDJOGADOR INT,
+	NOME VARCHAR(30),
+	ESTADO CHAR(2)
+);
+
+INSERT INTO JOGADORES VALUES(1,'GUERRERO','RS');
+INSERT INTO JOGADORES VALUES(2,'GABIGOL','RJ');
+INSERT INTO JOGADORES VALUES(3,'GANSO','RJ');
+INSERT INTO JOGADORES VALUES(4,'NENÊ', 'RJ');
+INSERT INTO JOGADORES VALUES(5,'LOVE','SP');
+
+CREATE VIEW V_JOGADORES AS
+SELECT NOME, ESTADO
+FROM JOGADORES;
+
+SELECT * FROM V_JOGADORES;
+
+DELETE FROM V_JOGADORES
+WHERE NOME = 'GUERRERO';
+
+INSERT INTO V_JOGADORES VALUES('GUERRERO','RS');
+
+SELECT * FROM JOGADORES;
+
+SELECT * FROM JOGADORES;
++-----------+----------+--------+
+| IDJOGADOR | NOME     | ESTADO |
++-----------+----------+--------+
+|         2 | GABIGOL  | RJ     |
+|         3 | GANSO    | RJ     |
+|         4 | NENÊ     | RJ     |
+|         5 | LOVE     | SP     |
+|      NULL | GUERRERO | RS     |
++-----------+----------+--------+
+
+SELECT * FROM V_RELATORIO
+WHERE SEXO = 'F';
+
+
+/* ORDER BY */
+
+CREATE TABLE ALUNOS (
+	NUMERO INT,
+	NOME VARCHAR(10)
+);
+
+
+INSERT INTO ALUNOS VALUES(1,'JOAO');
+INSERT INTO ALUNOS VALUES(1,'MARIA');
+INSERT INTO ALUNOS VALUES(2,'ZOE');
+INSERT INTO ALUNOS VALUES(2,'ANDRE');
+INSERT INTO ALUNOS VALUES(3,'CLARA');
+INSERT INTO ALUNOS VALUES(1,'CLARA');
+INSERT INTO ALUNOS VALUES(4,'MAFRA');
+INSERT INTO ALUNOS VALUES(5,'JANAINA');
+INSERT INTO ALUNOS VALUES(1,'JANAINA');
+INSERT INTO ALUNOS VALUES(3,'MARCELO');
+INSERT INTO ALUNOS VALUES(4,'GIOVANI');
+INSERT INTO ALUNOS VALUES(5,'ANTONIO');
+INSERT INTO ALUNOS VALUES(6,'ANA');
+INSERT INTO ALUNOS VALUES(6,'VIVIANE');
+
+SELECT * FROM ALUNOS
+ORDER BY NUMERO;
+
+SELECT * FROM ALUNOS
+ORDER BY 1;
+
+SELECT * FROM ALUNOS
+ORDER BY 2;
+
+/* ORDENANDO POR MAIS DE UMA COLUNA */
+
+SELECT * FROM ALUNOS
+ORDER BY 1;
+
+SELECT * FROM ALUNOS
+ORDER BY NUMERO, NOME;
+
+SELECT * FROM ALUNOS
+ORDER BY 1, 2;
+
+/* MESCLANDO ORDER BY COM PROJECAO */
+
+--> NAO FUNCIONA COM INDEXACAO
+SELECT NOME FROM ALUNOS
+ORDER BY 1, 2; 
+
+--> FACA ASSIM
+SELECT NOME FROM ALUNOS
+ORDER BY NUMERO, NOME;
+
+
+/* ORDER BY DESC / ASC */
+
+SELECT * FROM ALUNOS
+ORDER BY 1, 2;
+
+SELECT * FROM ALUNOS
+ORDER BY 1 ASC;
+
+SELECT * FROM ALUNOS
+ORDER BY 1 DESC;
+
+SELECT * FROM ALUNOS
+ORDER BY 1, 2 DESC;
+
+SELECT * FROM ALUNOS
+ORDER BY 1 DESC, 2 DESC;
+
+/* ORDENANDO COM JOINS */
+
+
+SELECT  C.NOME, 
+		C.SEXO, 
+		IFNULL(C.EMAIL,'CLIENTE SEM EMAIL') AS "E-MAIL", 
+		T.TIPO, 
+		T.NUMERO, 
+		E.BAIRRO, 
+		E.CIDADE, 
+		E.ESTADO
+FROM CLIENTE C 
+INNER JOIN TELEFONE T 
+ON C.IDCLIENTE = T.ID_CLIENTE 
+INNER JOIN ENDERECO E 
+ON C.IDCLIENTE = E.ID_CLIENTE
+ORDER BY 1;
+
+SHOW TABLES;
+
+SELECT * FROM V_RELATORIO
+ORDER BY 1;
